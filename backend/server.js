126061fdee8239929eb5d70417499eb231ae396a
tanay9098 <tanaydwivedi678@gmail.com>
2026-02-01 17:59:42 +0530

    fix:corrected while referring to environment variables

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 // connect mongodb
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=> console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error', err));
 
--- backend/utils/webpush.js
+++ backend/utils/webpush.js
@@ -1,5 +1,5 @@
 const webpush = require('web-push');
 
-webpush.setVapidDetails(process.env.VAPID_EMAIL || 'mailto:admin@example.com', process.env.VAPID_PUBLIC, process.env.VAPID_PRIVATE);
+webpush.setVapidDetails(process.env.VAPID_EMAIL || 'mailto:admin@example.com', process.env.VAPID_PUBLIC_KEY, process.env.VAPID_PRIVATE_KEY);
 
 module.exports = webpush;
