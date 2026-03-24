4e91a7e3d30fe5c3abe895b5e1afc0cc925e7938
tanay9098 <tanaydwivedi678@gmail.com>
2026-03-24 10:09:41 +0530

    Remove backend folder from repo

--- backend/models/Subscription.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const subSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  subscription: Object
-});
-
-module.exports = mongoose.model('Subscription', subSchema);
