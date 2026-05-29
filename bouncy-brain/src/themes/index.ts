37e5abdd175b1ef00dabea6e29084129c4ace671
tanay9098 <tanaydwivedi678@gmail.com>
2026-05-29 11:12:23 +0530

    Delete bouncy-brain directory

--- bouncy-brain/src/themes/index.ts
+++
@@ -1,4 +0,0 @@
-export { useTheme } from '../hooks/useTheme'
-export type { ThemeName } from '../stores/themeStore'
-
-export const THEME_NAMES = ['calm', 'focus', 'night', 'highContrast', 'minimal'] as const
--- bouncy-brain/src/utils/sound.js
+++
@@ -1,6 +0,0 @@
-import { Howl } from "howler";
-
-export const tabAlertSound = new Howl({
-  src: ["/sounds/tab-alert.mp3"],
-  volume: 0.5
-});
